9cc7e2c2a7836ed408da2af2e7e8ca252fcf84c9
Jason Alberto <alberto.jason@gmail.com>
2024-01-21 21:16:33 -0500

    Add tests to verify adding laps and reseting timer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -40,7 +40,9 @@ describe('Stopwatch App', () => {
     expect(timeElement).not.toHaveTextContent('00:00.00');
     expect(timeElement).toHaveTextContent('00:00.10');
     expect(startStopButton).toHaveTextContent('Stop');
+    expect(lapResetButton).toHaveTextContent('Lap');
 
+    // Trigger stop button click
     fireEvent.click(startStopButton);
 
     act(() => {
@@ -48,5 +50,48 @@ describe('Stopwatch App', () => {
     });    
 
     expect(timeElement).toHaveTextContent('00:00.10');
+  });
+
+  it('Should add laps and reset the timer', () => {
+    // Trigger start button click
+    fireEvent.click(startStopButton);
+
+    // Advance time by 100ms
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    // Check if the stopwatch time is updated
+    fireEvent.click(lapResetButton);
+
+    // Get and check laps
+    const currentLapTime = screen.getByText(/Current Lap:/i);
+    const lapTime1 = screen.getByText(/Lap 1: 00:00.10/i);
+    expect(currentLapTime).toBeInTheDocument();
+    expect(lapTime1).toBeInTheDocument();
+    expect(timeElement).toHaveTextContent('00:00.10');
+
+    act(() => {
+      jest.advanceTimersByTime(200);
+    });    
+
+    // Trigger stop button click
+    fireEvent.click(lapResetButton);
+    
+    // Get and check additional laps
+    const lapTime2 = screen.getByText(/Lap 2: 00:00.20/i);
+    expect(lapTime2).toBeInTheDocument();
+    expect(timeElement).toHaveTextContent('00:00.30');
+    
+    // Trigger stop and reset button click
+    fireEvent.click(startStopButton);
+    expect(lapResetButton).toHaveTextContent('Reset');
+    fireEvent.click(lapResetButton);
+
+    // Confirm reset render
+    expect(timeElement).toHaveTextContent('00:00.00');
+    expect(currentLapTime).not.toBeInTheDocument();
+    expect(lapTime1).not.toBeInTheDocument();
+    expect(lapTime2).not.toBeInTheDocument();
   });
 });
